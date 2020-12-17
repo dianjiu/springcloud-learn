1f9eaa2ae20f46e9c56ba5d095511398f407f616
dianjiu <dianjiu@dianjiu.cc>
2020-12-17 16:23:14 +0800

    完成spring cloud eureka工程

--- 01-spring-cloud-eureka/src/main/java/co/dianjiu/eureka/Application.java
+++ 01-spring-cloud-eureka/src/main/java/co/dianjiu/eureka/Application.java
@@ -2,11 +2,19 @@ package co.dianjiu.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
+        System.setProperty("cfg.env","local");
         SpringApplication.run(Application.class, args);
     }
 
