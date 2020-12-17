625b2a3a28f26f4e71b67b347d7946e3b84a9754
dianjiu <dianjiu@dianjiu.cc>
2020-12-17 18:10:12 +0800

    完成服务的提供者和服务的消费者调用实例

--- 03-spring-cloud-server-consumer-template/src/main/java/co/dianjiu/template/config/RestConfig.java
+++ 03-spring-cloud-server-consumer-template/src/main/java/co/dianjiu/template/config/RestConfig.java
@@ -1,12 +1,21 @@
 package co.dianjiu.template.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestConfig {
+
+    /**
+     * 注入 RestTemplate
+     * 并用 @LoadBalanced 注解，用负载均衡策略请求服务提供者
+     * 这是 Spring Ribbon 的提供的能力
+     * @return
+     */
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate;
