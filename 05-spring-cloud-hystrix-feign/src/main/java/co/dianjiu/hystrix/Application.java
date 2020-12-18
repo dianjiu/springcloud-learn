4dae400f4cd81c540795bd212f8807fc0b7875ed
dianjiu <dianjiu@dianjiu.cc>
2020-12-18 15:03:46 +0800

    整合Hystrix通过Fallback和FallbackFactory两种方式实现熔断降级

---
+++ 05-spring-cloud-hystrix-feign/src/main/java/co/dianjiu/hystrix/Application.java
@@ -0,0 +1,17 @@
+package co.dianjiu.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableEurekaClient
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
