30b6ccc890771671b00e75d20b403e9e5cd19405
dianjiu <dianjiu@dianjiu.cc>
2020-12-21 10:30:44 +0800

    优化配置

--- 07-spring-cloud-hystrix-feign-turbine/src/main/java/co/dianjiu/hystrix/Application.java
+++ 07-spring-cloud-hystrix-feign-turbine/src/main/java/co/dianjiu/hystrix/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
-@EnableTurbine
+@EnableTurbineStream
 @EnableEurekaClient
 @EnableHystrixDashboard
 @SpringBootApplication
