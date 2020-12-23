36234a074195630301bfd842f26bcb68c8d4ece2
dianjiu <dianjiu@dianjiu.cc>
2020-12-23 18:45:39 +0800

    springcloud 整合config git eureka

--- 09-spring-cloud-config-git-client/src/main/java/co/dianjiu/client/Application.java
+++ 09-spring-cloud-config-git-client/src/main/java/co/dianjiu/client/Application.java
@@ -2,9 +2,7 @@ package co.dianjiu.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
