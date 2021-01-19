6f825d0519d8a0d4e68b351d87405facdb4aa052
dianjiu <dianjiu@dianjiu.cc>
2021-01-19 18:05:31 +0800

    更新网关gateway笔记

--- 24-spring-cloud-gateway/src/main/java/co/dianjiu/gateway/Application.java
+++ 24-spring-cloud-gateway/src/main/java/co/dianjiu/gateway/Application.java
@@ -2,12 +2,22 @@ package co.dianjiu.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        System.out.println("请访问 http://localhost:18023/get");
     }
 
+    /*@Bean
+    RouteLocator routeLocator(RouteLocatorBuilder builder){
+        return builder.routes().
+                route("gongjie",j -> j.path("/get").uri("http://point9.top"))
+                .build();
+    }*/
 }
